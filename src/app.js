62b4ec1a5e601452ffaa1ad9ee6dfac395adb0ef
Michael Knopke <knopkem@gmail.com>
2020-04-30 15:54:36 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import './utils.js'
+
+console.log('app is running');
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js',
+    './css/app.css'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+     contentBase: './public',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
